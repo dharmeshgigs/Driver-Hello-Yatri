bc7f68d7a9f5f9ce0ddf1c0618902fc2c1d66ba7
HiraparaPriyanka <priyankahirapara2596@gmail.com>
2024-07-27 18:45:38 +0530

    Integrated Pusher

--- app/src/main/java/com/helloyatri/ui/home/HomeActivity.kt
+++ app/src/main/java/com/helloyatri/ui/home/HomeActivity.kt
@@ -28,8 +28,17 @@ import com.helloyatri.ui.home.fragment.RideActivityFragment
 import com.helloyatri.ui.home.sidemenu.SideMenu
 import com.helloyatri.ui.home.sidemenu.SideMenuAdapter
 import com.helloyatri.ui.home.sidemenu.SideMenuTag
+import com.helloyatri.utils.Constants.YOUR_APP_CLUSTER
+import com.helloyatri.utils.Constants.YOUR_APP_KEY
 import com.helloyatri.utils.extension.loadImageFromServerWithPlaceHolder
+import com.pusher.client.Pusher
+import com.pusher.client.PusherOptions
+import com.pusher.client.channel.Channel
+import com.pusher.client.connection.ConnectionEventListener
+import com.pusher.client.connection.ConnectionStateChange
+import com.pusher.client.util.HttpChannelAuthorizer
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class HomeActivity : BaseActivity() {
@@ -59,22 +68,65 @@ class HomeActivity : BaseActivity() {
         setUpSideMenuClickListener()
         load(HomeFragment::class.java).replace(false)
         initObservers()
+        setUpPushUp()
+
 //        homeAcitivtyBinding.navigationDrawerContent.imageViewUserProfile.loadImageFromServerWithPlaceHolder(
 //                "https://plus.unsplash.com/premium_photo-1669047668540-9e1712e29f1f?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
     }
 
+    private fun setUpPushUp() {
+        val map = mutableMapOf<String, String>()
+        map["Accept"] = "application/json"
+        map["Authorization"] = "Bearer ".plus(appSession.userSession)
+
+        val channelAuthorizer = HttpChannelAuthorizer("http://3.111.159.32/api/pusher/auth")
+        channelAuthorizer.setHeaders(map)
+
+        val options = PusherOptions().setCluster(YOUR_APP_CLUSTER).setUseTLS(true).setChannelAuthorizer(channelAuthorizer)
+        val pusher = Pusher(YOUR_APP_KEY, options)
+        var userId = appSession.userId
+        Log.i("TAG", "setUpPushUp: "+userId)
+        pusher.connect(object : ConnectionEventListener {
+            override fun onConnectionStateChange(change: ConnectionStateChange) {
+                Log.i("TAG", "onConnectionStateChange: "+change.currentState )
+                Log.i("TAG", "onConnectionStateChange: "+change.previousState )
+                println(
+                    "State changed to " + change.currentState +
+                            " from " + change.previousState
+                )
+            }
+
+            override fun onError(message: String?, code: String?, e: Exception?) {
+                println("There was a problem connecting!")
+                Log.i("TAG", "onError: "+message)
+            }
+        })
+
+
+        val channel: Channel = pusher.subscribe("private-driver.$userId")
+        channel.bind(
+            "NewRideRequest"
+        ) { data ->
+            println("Received event with data: $data")
+            Log.i("TAG", "setUpPushUp: "+data.data)
+        }
+
+    }
+
     private fun initObservers() {
-        apiViewModel.getDriverProfileLiveData.observe(this){resource ->
-            when(resource.status){
+        apiViewModel.getDriverProfileLiveData.observe(this) { resource ->
+            when (resource.status) {
                 Status.SUCCESS -> {
                     val response =
                         Gson().fromJson(resource.data.toString(), DriverResponse::class.java)
                     setUserData(response.data)
-                    Log.i("TAG", "initObservers: "+response.data.toString())
+                    Log.i("TAG", "initObservers: " + response.data.toString())
                 }
+
                 Status.ERROR -> {
 
                 }
+
                 Status.LOADING -> {
 
                 }
@@ -84,7 +136,9 @@ class HomeActivity : BaseActivity() {
     }
 
     private fun setUserData(data: Driver?) = with(homeAcitivtyBinding) {
-        navigationDrawerContent.imageViewUserProfile.loadImageFromServerWithPlaceHolder(data?.profileImage ?: "")
+        navigationDrawerContent.imageViewUserProfile.loadImageFromServerWithPlaceHolder(
+            data?.profileImage ?: ""
+        )
         navigationDrawerContent.textViewUserName.text = buildString {
             append("Hello \n ")
             append(data?.name)
@@ -107,16 +161,21 @@ class HomeActivity : BaseActivity() {
         }
         sideMenuList.clear()
         sideMenuList.add(
-                SideMenu(sideMenuName = "Edit Profile", sideMenuTag = SideMenuTag.EDIT_PROFILE))
+            SideMenu(sideMenuName = "Edit Profile", sideMenuTag = SideMenuTag.EDIT_PROFILE)
+        )
         sideMenuList.add(
-                SideMenu(sideMenuName = "Ride Activity", sideMenuTag = SideMenuTag.RIDE_ACTIVITY))
+            SideMenu(sideMenuName = "Ride Activity", sideMenuTag = SideMenuTag.RIDE_ACTIVITY)
+        )
         sideMenuList.add(SideMenu(sideMenuName = "Payment", sideMenuTag = SideMenuTag.PAYMENT))
         sideMenuList.add(
-                SideMenu(sideMenuName = "Saved Address", sideMenuTag = SideMenuTag.SAVED_ADDRESS))
+            SideMenu(sideMenuName = "Saved Address", sideMenuTag = SideMenuTag.SAVED_ADDRESS)
+        )
         sideMenuList.add(
-                SideMenu(sideMenuName = "Preferences", sideMenuTag = SideMenuTag.PREFERENCES))
+            SideMenu(sideMenuName = "Preferences", sideMenuTag = SideMenuTag.PREFERENCES)
+        )
         sideMenuList.add(
-                SideMenu(sideMenuName = "Help Center", sideMenuTag = SideMenuTag.HELP_CENTER))
+            SideMenu(sideMenuName = "Help Center", sideMenuTag = SideMenuTag.HELP_CENTER)
+        )
         sideMenuList.add(SideMenu(sideMenuName = "Documents", sideMenuTag = SideMenuTag.DOCUMENTS))
         sideMenuList.add(SideMenu(sideMenuName = "Logout", sideMenuTag = SideMenuTag.LOGOUT))
         sideMenuAdapter.setItems(sideMenuList, 1)
